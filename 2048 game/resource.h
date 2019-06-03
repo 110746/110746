868d59c8ceecec349530672647b2f6da20f60971
110746 <49853288+110746@users.noreply.github.com>
2019-06-03 21:35:22 +0800

    res

---
+++ 2048 game/resource.h
@@ -0,0 +1,35 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+//
+#define IDS_DESCRIPTION                 1
+#define MAXFILELEN                      13
+#define TITLEBARNAMELEN                 40
+#define APPNAMEBUFFERLEN                40
+#define ID_APP                          100
+#define IDB_PNG1                        106
+#define IDB_PNG2                        110
+#define IDB_PNG3                        111
+#define BUFFLEN                         255
+#define idsIsPassword                   1000
+#define idsIniFile                      1001
+#define idsScreenSaver                  1002
+#define idsPassword                     1003
+#define idsDifferentPW                  1004
+#define idsChangePW                     1005
+#define idsBadOldPW                     1006
+#define idsAppName                      1007
+#define idsNoHelpMemory                 1008
+#define idsHelpFile                     1009
+#define idsDefKeyword                   1010
+#define DLG_SCRNSAVECONFIGURE           2003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        112
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
